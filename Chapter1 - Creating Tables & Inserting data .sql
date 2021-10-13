9e351a75fd5cac9cc11f2c4c2ec1866f13f24b9a
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-14 01:13:53 +0530

    Update Chapter1 - Creating Tables & Inserting data .sql

--- Chapter1 - Creating Tables & Inserting data .sql
+++ Chapter1 - Creating Tables & Inserting data .sql
@@ -18,6 +18,8 @@ drop table transactions;
 
 -- 3. Inserting data into a table;
 
+INSERT INTO transactions (id, user_id, created_at, product_id, quantity)
+VALUES (103, 1,'2021-09-01',1203,4), (104, 2,'2021-09-02',1203,8), (105, 2, '2021-09-01', 1204, 5), (106, 3, '2021-09-02', 1204, 7);
 
 
 
