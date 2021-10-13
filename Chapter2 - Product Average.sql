db945f6dfa0fb0471e538b410237e12eec63dc3f
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-14 00:42:16 +0530

    Update Chapter2 - Product Average.sql

--- Chapter2 - Product Average.sql
+++ Chapter2 - Product Average.sql
@@ -1,5 +1,7 @@
-------------------------------- Calculating Product Average --------------------------
+------------------------------- Calculating Product Average
 
+--- Loading the data
+------------------------------------------------------------------------------------------------------------------------------------------
 -- 1.  Creating "transactions" table;
 
 create table transactions
@@ -11,7 +13,13 @@ create table transactions
    quantity int
 );
 
--- 2. Create "products" table
+-- 2. Insert data into "transactions" table
+
+INSERT INTO transactions (id, user_id, created_at, product_id, quantity)
+VALUES (103, 1203,'2021-09-01',1,4), (104, 1203,'2021-09-02',2,8), (105, 1204, '2021-09-01', 1, 5), (106, 1204, '2021-09-02', 3, 7);
+
+
+-- 3. Create "products" table
 
 create table products
 (
@@ -20,12 +28,13 @@ create table products
     price float
 );
 
--- 3. Insert data into "transactions" table
-
-INSERT INTO transactions (id, user_id, created_at, product_id, quantity)
-VALUES (103, 1203,'2021-09-01',1,4), (104, 1203,'2021-09-02',2,8), (105, 1204, '2021-09-01', 1, 5), (106, 1204, '2021-09-02', 3, 7);
-
 --4. Insert data into "products" table
 
 INSERT INTO products (id, name, price)
 VALUES (1203, 'Apple', 40.5), (1204, 'Orange',8), (1205, 'Banana', 2.5), (1206,'Custard Apple', 37);
+
+------------------------------------------------------------------------------------------------------------------------------------------
+--- SQL Query to calculate the Average transactional value of the product
+
+
+
