05be1c94fbfd511581759d132e952e769546d018
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-14 11:13:23 +0530

    Update Chapter4- Identifying Third Purchase.sql

--- Chapter4- Identifying Third Purchase.sql
+++ Chapter4- Identifying Third Purchase.sql
@@ -1,4 +1,4 @@
-------------------------------- Calculating Product Average
+------------------------------- Identifying Third Purchase
 
 ------- Given the revenue transactions table above, write a query that finds the third purchase of every user.
 ------------------------------------------------------------------------------------------------------------------------------------------
@@ -21,7 +21,7 @@ INSERT INTO transactions (id, user_id, created_at, product_id, quantity)
 VALUES (103, 1,'2021-09-01',1203,4), (104, 2,'2021-09-02',1203,8), (105, 2, '2021-09-01', 1204, 5), (106, 3, '2021-09-02', 1204, 7);
 
 ------------------------------------------------------------------------------------------------------------------------------------------
--                                      SQL Query to calculate the Average transactional value of the product
+-                                      SQL Query to Identify the Third Purchase
 ------------------------------------------------------------------------------------------------------------------------------------------
 Actual solution:
 
