47bfd8411706322fea8bc1ad0bd3577ab653ad85
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-14 17:12:02 +0530

    Update and rename Easy Module - .sql to Easy Module - Manager Team Sizes.sql

--- Easy Module - Manager Team Sizes.sql
+++ Easy Module - Manager Team Sizes.sql
@@ -1,14 +1,14 @@
-------------------------------- 
+------------------------------- Manager Team Sizes
 
-------- 
+------- Write a query to identify the manager with the biggest team size. You may assume there is only one manager with the largest team size
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 -                                                            Loading the data
 ------------------------------------------------------------------------------------------------------------------------------------------
 
--- 1.  Creating "transactions" table;
+-- 1.  Creating "employees" table;
 
-create table transactions
+create table employees
 (
    id int,
    user_id int,
@@ -17,14 +17,14 @@ create table transactions
    quantity int
 );
 
--- 2. Insert data into "transactions" table
+-- 2. Insert data into "employees" table
 
-INSERT INTO transactions (id, user_id, created_at, product_id, quantity)
+INSERT INTO employees (id, user_id, created_at, product_id, quantity)
 VALUES (103, 1,'2021-09-01',1203,4), (104, 2,'2021-09-02',1203,8), (105, 2, '2021-09-01', 1204, 5), (106, 3, '2021-09-02', 1204, 7);
 
--- 3.  Creating "transactions" table;
+-- 3.  Creating "managers" table;
 
-create table transactions
+create table managers
 (
    id int,
    user_id int,
@@ -33,9 +33,9 @@ create table transactions
    quantity int
 );
 
--- 4. Insert data into "transactions" table
+-- 4. Insert data into "managers" table
 
-INSERT INTO transactions (id, user_id, created_at, product_id, quantity)
+INSERT INTO managers (id, user_id, created_at, product_id, quantity)
 VALUES (103, 1,'2021-09-01',1203,4), (104, 2,'2021-09-02',1203,8), (105, 2, '2021-09-01', 1204, 5), (106, 3, '2021-09-02', 1204, 7);
 
 ------------------------------------------------------------------------------------------------------------------------------------------
