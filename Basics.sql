b187be478e151f697fc56a04abacaf344770e9e8
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-14 18:54:17 +0530

    Update Basics.sql

--- Basics.sql
+++ Basics.sql
@@ -71,4 +71,17 @@ select  distinct city from station where lower(city) REGEXP '^[aeiou]' and lower
 select distinct city from station where lower(substr(city, length(city), length(city))) not in ('a','e','i','o','u')
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--9  Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+--   If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+TRICK!: Right - last ; Left - First : "slicing of strings"
+
+select Name from STUDENTS where Marks > 75 
+order by RIGHT(Name, 3), ID;   
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
 
