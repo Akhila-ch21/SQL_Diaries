a877faade6405f07eb402f0cb1bcd7f4ee2d8842
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-20 22:54:09 +0530

    Update SQL - Material.sql

--- SQL - Material.sql
+++ SQL - Material.sql
@@ -1,13 +1,16 @@
 
 /*.                                                                 SQL - Structured Query Laungage
-SQL COMMANDS:
-  - DDL (Data defintion language)
-  - DML (Data Manipulation language)
-  - DCL (Data control language)
-  - TCL (Transaction control language)
-  - DQL (Data Query Language)
+
+  SQL COMMANDS:
+
+    - DDL (Data defintion language)
+    - DML (Data Manipulation language)
+    - DCL (Data control language)
+    - TCL (Transaction control language)
+    - DQL (Data Query Language)
   
  Link: https://www.geeksforgeeks.org/sql-ddl-dql-dml-dcl-tcl-commands
+ 
  */
  
  -------------------------------------------------------------------------------------------------------------------------------------------------------------------
