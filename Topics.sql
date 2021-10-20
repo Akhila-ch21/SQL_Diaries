baab57a86fe55d07b8a53cb2af766ec6fa44de61
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-20 12:42:59 +0530

    Update Topics.sql

--- Topics.sql
+++ Topics.sql
@@ -18,7 +18,23 @@ Data Modelling:
      - Avg()
      - count(distinct())
      
-2. 	  
+2. Filters:
+    - Where clause
+
+3. GROUPBY
+
+4. WINDOW FUNCTIONS
+
+5. TOP Nth records 
+      - limit n-1, 1
+      
+6. Create & Inserting data into a table
+      - create a table();
+      - insert into table
+        values()();
+
+7. Delete a table:
+      - DROP TABLE table_name from db_name
 
 
 
