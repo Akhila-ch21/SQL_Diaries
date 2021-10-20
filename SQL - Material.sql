e3db370b82ed7325c33381fdeecf748baccc76d3
Akhilaa <82121181+Akhila-ch21@users.noreply.github.com>
2021-10-20 22:52:53 +0530

    Create SQL - Material.sql

---
+++ SQL - Material.sql
@@ -0,0 +1,12 @@
+
+/*
+SQL - Structured Query Laungage
+SQL COMMANDS
+  - DDL (Data defintion language)
+  - DML (Data Manipulation language)
+  - DCL (Data control language)
+  - TCL (Transaction control language)
+  - DQL (Data Query Language)
+  
+ Link: https://www.geeksforgeeks.org/sql-ddl-dql-dml-dcl-tcl-commands
+ */
